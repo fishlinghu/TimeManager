dc3344853024a110f32c4a2a3a045d61eb88b4c6
fishlinghu <fishlinghu@gmail.com>
2017-09-11 03:15:51 -0400

    Finish basic version of account creation

--- app/src/main/java/com/example/fishlinghu/timemanager/CreateAccountActivity.java
+++ app/src/main/java/com/example/fishlinghu/timemanager/CreateAccountActivity.java
@@ -1,13 +1,58 @@
 package com.example.fishlinghu.timemanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private DatabaseReference reference;
+    private FirebaseUser GoogleUser;
+    private String AccountEmail;
+
+    private EditText EditTextName;
+
+    private Button ButtonDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        GoogleUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        EditTextName = findViewById(R.id.editText_Name);
+
+        ButtonDone = findViewById(R.id.button_done);
+
+        AccountEmail = GoogleUser.getEmail();
+
+        ButtonDone.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                User NewUserData = new User(
+                        EditTextName.getText().toString(),
+                        GoogleUser.getPhotoUrl().toString()
+                );
+
+                reference.child("users").child( AccountEmail.replace(".",",") ).setValue(NewUserData);
+
+                Toast.makeText(v.getContext(), "Account created", Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
+                finish();
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fishlinghu/timemanager/User.java
@@ -0,0 +1,29 @@
+package com.example.fishlinghu.timemanager;
+
+/**
+ * Created by fishlinghu on 9/11/17.
+ */
+
+public class User {
+
+    private String name ;
+    private String photoUrl;
+
+    public User() {
+    }
+
+    public User(String name, String photoUrl) {
+
+        this.name = name;
+        this.photoUrl = photoUrl;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+}
